393950c33485c61a0b639f05ce55d247b2840d26
Richard Boyce <ningyifan1212@163.com>
2015-04-30 10:45:06 -0400

    migrates prototype from netbeans to maven, add readme

---
+++ maven-src/DIKB-Prototype/src/main/java/com/ddi/Drug.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ddi;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author cwm24
+ */
+public class Drug {
+    private ArrayList<String> drugNames;
+
+    /**
+     * @return the drugNames
+     */
+    public ArrayList<String> getDrugNames() {
+        return drugNames;
+    }
+
+    /**
+     * @param drugNames the drugNames to set
+     */
+    public void setDrugNames(ArrayList<String> drugNames) {
+        this.drugNames = drugNames;
+    }
+    
+}
